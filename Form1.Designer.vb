ea942889785803a8ea46b3c46b3f3b86b9b97e21
dsantamaria18 <dsantamaria18@gmail.com>
2016-02-06 18:24:18 +0100

    Textboxes added

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -26,11 +26,14 @@ Partial Class Form1
         Me.LabelRutaAdjuntos = New System.Windows.Forms.Label()
         Me.TextBoxRutaAdjuntos = New System.Windows.Forms.TextBox()
         Me.FolderBrowserDialogRutaAdjuntos = New System.Windows.Forms.FolderBrowserDialog()
+        Me.LabelSalida = New System.Windows.Forms.Label()
+        Me.TextBoxSalida = New System.Windows.Forms.TextBox()
+        Me.ButtonSalida = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ButtonRutaAdjuntos
         '
-        Me.ButtonRutaAdjuntos.Location = New System.Drawing.Point(336, 37)
+        Me.ButtonRutaAdjuntos.Location = New System.Drawing.Point(336, 36)
         Me.ButtonRutaAdjuntos.Name = "ButtonRutaAdjuntos"
         Me.ButtonRutaAdjuntos.Size = New System.Drawing.Size(24, 23)
         Me.ButtonRutaAdjuntos.TabIndex = 0
@@ -53,11 +56,39 @@ Partial Class Form1
         Me.TextBoxRutaAdjuntos.Size = New System.Drawing.Size(235, 20)
         Me.TextBoxRutaAdjuntos.TabIndex = 2
         '
+        'LabelSalida
+        '
+        Me.LabelSalida.AutoSize = True
+        Me.LabelSalida.Location = New System.Drawing.Point(50, 68)
+        Me.LabelSalida.Name = "LabelSalida"
+        Me.LabelSalida.Size = New System.Drawing.Size(39, 13)
+        Me.LabelSalida.TabIndex = 3
+        Me.LabelSalida.Text = "Salida:"
+        '
+        'TextBoxSalida
+        '
+        Me.TextBoxSalida.Location = New System.Drawing.Point(95, 65)
+        Me.TextBoxSalida.Name = "TextBoxSalida"
+        Me.TextBoxSalida.Size = New System.Drawing.Size(235, 20)
+        Me.TextBoxSalida.TabIndex = 4
+        '
+        'ButtonSalida
+        '
+        Me.ButtonSalida.Location = New System.Drawing.Point(336, 63)
+        Me.ButtonSalida.Name = "ButtonSalida"
+        Me.ButtonSalida.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonSalida.TabIndex = 5
+        Me.ButtonSalida.Text = "..."
+        Me.ButtonSalida.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(475, 262)
+        Me.Controls.Add(Me.ButtonSalida)
+        Me.Controls.Add(Me.TextBoxSalida)
+        Me.Controls.Add(Me.LabelSalida)
         Me.Controls.Add(Me.TextBoxRutaAdjuntos)
         Me.Controls.Add(Me.LabelRutaAdjuntos)
         Me.Controls.Add(Me.ButtonRutaAdjuntos)
@@ -71,5 +102,8 @@ Partial Class Form1
     Friend WithEvents LabelRutaAdjuntos As System.Windows.Forms.Label
     Friend WithEvents TextBoxRutaAdjuntos As System.Windows.Forms.TextBox
     Friend WithEvents FolderBrowserDialogRutaAdjuntos As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents LabelSalida As System.Windows.Forms.Label
+    Friend WithEvents TextBoxSalida As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonSalida As System.Windows.Forms.Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,48 @@
 ﻿Public Class Form1
 
+    Private Sub ButtonRutaAdjuntos_Click(sender As System.Object, e As System.EventArgs) Handles ButtonRutaAdjuntos.Click
+        Try
+            ' Configuración del FolderBrowserDialog  
+            With FolderBrowserDialogRutaAdjuntos
+                .Reset() ' resetea  
+                ' leyenda  
+                .Description = " Selecciona la carpeta con los adjuntos "
+                ' Path por defecto: Escritorio "  
+                .SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                .RootFolder = Environment.SpecialFolder.Desktop
+                ' deshabilita el botón " crear nueva carpeta "  
+                .ShowNewFolderButton = False
+                Dim ret As DialogResult = .ShowDialog ' abre el diálogo  
+                If ret = Windows.Forms.DialogResult.OK Then
+                    TextBoxRutaAdjuntos.Text = .SelectedPath
+                End If
+                .Dispose()
+            End With
+        Catch oe As Exception
+            MsgBox(oe.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub ButtonSalida_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSalida.Click
+        Try
+            ' Configuración del FolderBrowserDialog  
+            With FolderBrowserDialogRutaAdjuntos
+                .Reset() ' resetea  
+                ' leyenda  
+                .Description = " Selecciona la carpeta con los adjuntos "
+                ' Path por defecto: Escritorio "  
+                .SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                .RootFolder = Environment.SpecialFolder.Desktop
+                ' deshabilita el botón " crear nueva carpeta "  
+                .ShowNewFolderButton = False
+                Dim ret As DialogResult = .ShowDialog ' abre el diálogo  
+                If ret = Windows.Forms.DialogResult.OK Then
+                    TextBoxSalida.Text = .SelectedPath
+                End If
+                .Dispose()
+            End With
+        Catch oe As Exception
+            MsgBox(oe.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
 End Class
