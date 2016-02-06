4237d235cf8d347b02c0b4566799c1002db4e4d3
dsantamaria18 <dsantamaria18@gmail.com>
2016-02-06 17:43:40 +0100

    Added first form

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,17 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ButtonRutaAdjuntos = New System.Windows.Forms.Button()
+        Me.LabelRutaAdjuntos = New System.Windows.Forms.Label()
+        Me.TextBoxRutaAdjuntos = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialogRutaAdjuntos = New System.Windows.Forms.FolderBrowserDialog()
         Me.SuspendLayout()
+        '
+        'ButtonRutaAdjuntos
+        '
+        Me.ButtonRutaAdjuntos.Location = New System.Drawing.Point(336, 37)
+        Me.ButtonRutaAdjuntos.Name = "ButtonRutaAdjuntos"
+        Me.ButtonRutaAdjuntos.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonRutaAdjuntos.TabIndex = 0
+        Me.ButtonRutaAdjuntos.Text = "..."
+        Me.ButtonRutaAdjuntos.UseVisualStyleBackColor = True
+        '
+        'LabelRutaAdjuntos
+        '
+        Me.LabelRutaAdjuntos.AutoSize = True
+        Me.LabelRutaAdjuntos.Location = New System.Drawing.Point(12, 42)
+        Me.LabelRutaAdjuntos.Name = "LabelRutaAdjuntos"
+        Me.LabelRutaAdjuntos.Size = New System.Drawing.Size(77, 13)
+        Me.LabelRutaAdjuntos.TabIndex = 1
+        Me.LabelRutaAdjuntos.Text = "Ruta Adjuntos:"
+        '
+        'TextBoxRutaAdjuntos
+        '
+        Me.TextBoxRutaAdjuntos.Location = New System.Drawing.Point(95, 39)
+        Me.TextBoxRutaAdjuntos.Name = "TextBoxRutaAdjuntos"
+        Me.TextBoxRutaAdjuntos.Size = New System.Drawing.Size(235, 20)
+        Me.TextBoxRutaAdjuntos.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(475, 262)
+        Me.Controls.Add(Me.TextBoxRutaAdjuntos)
+        Me.Controls.Add(Me.LabelRutaAdjuntos)
+        Me.Controls.Add(Me.ButtonRutaAdjuntos)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Prestashop Helper"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents ButtonRutaAdjuntos As System.Windows.Forms.Button
+    Friend WithEvents LabelRutaAdjuntos As System.Windows.Forms.Label
+    Friend WithEvents TextBoxRutaAdjuntos As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialogRutaAdjuntos As System.Windows.Forms.FolderBrowserDialog
 
 End Class
